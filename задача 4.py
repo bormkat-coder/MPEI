35003ce166b23cc64db74027c113779543abdd05
bormk <bormkat@gmail.com>
2026-03-05 22:35:17 +0300

    правки

--- задача 4.py
+++ задача 4.py
@@ -1,12 +1,14 @@
 #задача 4
 #зададим произвольный словарь
-dict = {
-    "кошка": "животное",
-    "Python": "язык",
-    "малина": "ягода",
-    "банан": "фрукт"
-}
+my_dict = {}
+n = int(input("Сколько пар добавить: "))
+for i in range(n):
+    kl = input("Введите ключ: ")
+    znach = input("Введите значение: ")
+    my_dict[kl] = znach #добавим пару в словарь
+print(my_dict)
+
 dict2 = {}
-for kl, znach in dict.items():
+for kl, znach in my_dict.items():
     dict2[znach] = kl
 print(dict2)
